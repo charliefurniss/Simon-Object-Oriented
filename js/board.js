42730dfc6cbf5b4e45f429797d562396aec448e3
Charlie Furniss <charlie@settled.co.uk>
2017-12-22 12:09:06 +0000

    starting to refactor board

--- js/board.js
+++ js/board.js
@@ -1,135 +1,144 @@
 var Board = function(){
 
-	function hideElement(id){
-		$("#" + id).css("display", "none");
-	}
-
-	function showElement(id){
-		$("#" + id).css("display", "block");
-	}
-
-	function slideDownElement(id) {
-		$("#" + id).slideDown();
-	}
-
-	function changeText(id, message){
-		$("#" + id).text(message);
-	}
-
-	function displayPlayerName(name){
-		//adds name to h2 tage and the reveals the h2 previsouly hidden h2 tag
-		$("#name").text(name + ": 0");
-		$("#name").slideDown(name + ": 0");
-	}
-
-	function flashOn(buttonToFlash){
-		$("." + buttonToFlash).css("background-color", $("." + buttonToFlash).attr('id'));
-
-	}
-
-	function flashOff(buttonToFlash){
-		$("." + buttonToFlash).css("background-color", $("." + buttonToFlash).attr('value'));
-	}
-
-	function makeSound(soundURL){
-		//plays the relevant sound when called either by the player or the computer
-		mySound = soundManager.createSound({        
-		  "url": soundURL
-		});
-		mySound.play();
-	};
-
 	this.clearNameInputWindow = function(){
-		//on "RETURN" clears the infoWindow, disables keypress and calls setUpGame and startGame
-		hideElement("enterName");
-		hideElement("nameInput");
-		$("#nameInput").val("");
-		$(document).unbind("keypress");
+		hideElement('enterName');
+		hideElement('nameInput');
+		$('#nameInput').val('');
+		$(document).unbind('keypress');
 	}
 
 	this.showInfoWindow = function(){
-		hideElement("board");
-		slideDownElement("infoWindow");
+		hideElement('board');
+		slideDownElement('infoWindow');
 	}
 
-	this.intro = function(){
+	this.setUpNameInputView = function(){
 		this.showInfoWindow();
-		slideDownElement("enterName");
-		slideDownElement("nameInput");
-		$("#nameInput").focus();  			//shows infoWindow	
+		slideDownElement('enterName');
+		slideDownElement('nameInput');
+		$('#nameInput').focus();  			//shows infoWindow	
 	}
 
 	this.setUpGame = function(playerName){
 		displayPlayerName(playerName);
 		//removes infoWindow
-		hideElement("infoWindow");
+		hideElement('infoWindow');
 		//after delay, shows the game board
 		setTimeout(function(){
-			showElement("board");
+			showElement('board');
 		}, 1000);
 	}
 
 	this.generateSimonButton = function(){
 		//chooses computer button randomly and assigns it to simonButton
 		var randomNumber = Math.random() * 4;
+
 		if (randomNumber <= 1) {
-			simonButton = $(".red");
+			simonButton = $('.red');
 		} else if (randomNumber <= 2) {
-			simonButton = $(".green");
+			simonButton = $('.green');
 		} else if (randomNumber <= 3) {
-			simonButton = $(".blue");
+			simonButton = $('.blue');
 		} else {
-			simonButton = $(".yellow");
+			simonButton = $('.yellow');
 		}
+
 		return simonButton;
 	}
 
-		this.simonFlashSound = function(simonLog){
+	this.executeSimonTurn = function(simonLog){
 		//cycles through the computer log to make the 
 		//correct button flash in the sequence
 		for (i = 0; i < simonLog.length; i++) {
 			//this function allows the timeout functions to work within the for loop
 			(function(i){
-			  setTimeout(function(){
-			    //flash on       
-		  		flashOn(simonLog[i]);
-		  		makeSound($("." + simonLog[i]).attr("url"));
-		  		setTimeout(function(){
-		  			//flash off
-			  		flashOff(simonLog[i]);
-			  	}, 300);				  			
+				setTimeout(function(){
+					//flash on       
+					flashOn(simonLog[i]);
+					makeSound($('.' + simonLog[i]).attr('url'));
+					setTimeout(function(){
+						//flash off
+						flashOff(simonLog[i]);
+					}, 300);				  			
 				}, 500 * i);
 			}(i));
 		};
-
-		this.registerPlayerClick = function(buttonClass){
-			//flashes and sounds on every player click
-			flashOn(buttonClass);
-			makeSound($("." + buttonClass).attr("url"));
-			setTimeout(function(){
-				flashOff(buttonClass);
-			}, 300);
-		};
-
-		this.increasePlayerScoreDisplay = function(playerName, round){
-			$("h2").text(playerName + ": " + (round));
-		}
-
-		this.alertError = function(){
-			var errorSoundURL = "audio/errorSound.mp3";
-			makeSound(errorSoundURL);
-			// replaces game board with info window
-			this.showInfoWindow();
-			changeText("message", "whoops!");
-			setTimeout(function(){
-				$("#message").slideUp(500, function(){
-					changeText("message", "");
-					showElement("message");
-				});
-				changeText("question", "do you want to play again (y/n)?");
-				slideDownElement("question");
-			}, 2000);
-		}
-
 	}
+
+	this.registerPlayerClick = function(buttonClassName){
+		var soundUrl = generateSoundUrl(buttonClassName);
+
+		makeSound(soundUrl);
+		makeFlash(buttonClassName);
+	};
+
+	function generateSoundUrl (buttonClassName) {
+		return $('.' + buttonClassName).attr('url');
+	}
+
+	function makeFlash (buttonClassName) {
+		var elementToFlash = $('.' + buttonClassName);
+		flashOn(elementToFlash);
+		setTimeout(function(){
+			flashOff(elementToFlash);
+		}, 300);
+	}
+
+	function flashOn(elementToFlash){
+		elementToFlash.css('background-color', elementToFlash.attr('id'));
+	}
+
+	function flashOff(elementToFlash){
+		elementToFlash.css('background-color', elementToFlash.attr('value'));
+	}
+
+	this.increasePlayerScoreDisplay = function(round){
+		$('#playerScore').text(round);
+	}
+
+	this.alertError = function(){
+		var errorSoundURL = 'audio/errorSound.mp3';
+		makeSound(errorSoundURL);
+		// replaces game board with info window
+		this.showInfoWindow();
+		changeText('message', 'whoops!');
+		setTimeout(function(){
+			$('#message').slideUp(500, function(){
+				changeText('message', '');
+				showElement('message');
+			});
+			changeText('question', 'do you want to play again (y/n)?');
+			slideDownElement('question');
+		}, 2000);
+	}
+
+	function hideElement(id){
+		$('#' + id).css('display', 'none');
+	}
+
+	function showElement(id){
+		$('#' + id).css('display', 'block');
+	}
+
+	function slideDownElement(id) {
+		$('#' + id).slideDown();
+	}
+
+	function changeText(id, message){
+		$('#' + id).text(message);
+	}
+
+	function displayPlayerName(name){
+		//adds name to h2 tage and the reveals the h2 previsouly hidden h2 tag
+		$('#playerName').text(name);
+		$('h2').slideDown();
+	}
+
+	function makeSound(soundURL){
+		mySound = soundManager.createSound({        
+			'url': soundURL
+		});
+		mySound.play();
+	};
+
 }
--- js/gameplay.js
+++ js/gameplay.js
@@ -1,180 +1,183 @@
 $(document).ready(function() {
 
+	var board = new Board();
+	var player = new Player();
+	var simon = new Simon();
+	var game = new Game();
+
 	soundManager.setup({
 	  url: 'js/soundmanager/swf',
-	  flashVersion: 9,
-	  onready: init
+	  flashVersion: 9
 	});
 
 	function init(){
 
-		var board = new Board();
-		var player = new Player();
-		var simon = new Simon();
-		var game = new Game();
-
 		startIntro();
 
-		function startIntro(){
-			board.intro();
-			setUpPlayerNameListener();
-		}	
+	}
 
-		function setUpPlayerNameListener(){
-			$(document).keypress(function(event){
-				setUpPlayerNameHandler(event);
-			})
-		};
+	function startIntro(){
+		board.setUpNameInputView();
+		setUpPlayerNameListener();
+	}	
 
-		function setUpPlayerNameHandler (event) {
-			player.name = $('#nameInput').val().toLowerCase();
-			if (event.which == 13) {
-				startGame();
-			}
-		}
+	function setUpPlayerNameListener(){
+		$(document).keypress(function(event){
+			setUpPlayerNameHandler(event);
+		})
+	};
 
-		function startGame(){
-			board.clearNameInputWindow();
-			board.setUpGame(player.name);
-  		simonTurn();	  	
-		}
-
-		function playerClick(){
-			$('input').each(function(){
-				$(this).on('click', function(){
-					preventMultipleClick();
-					handlePlayerClick($(this));
-				})
-			})
-		};
-
-		function preventMultipleClick () {
-			$('input').off('click');
-		}
-
-		function handlePlayerClick (selectedElement) {
-			board.registerPlayerClick(selectedElement.attr('class'));
-			player.clickLog.push(selectedElement.attr('class'));
-			assessClick();
-		}
-
-		function assessClick(){
-			isPlayerClickCorrect() ? handleCorrectChoice() : handleIncorrectChoice();	
-		}
-
-		function isPlayerClickCorrect () {
-			return (player.clickLog[player.clickNumber] == simon.clickLog[player.clickNumber]);
-		}
-
-		function handleCorrectChoice () {
-			isStillPlayersTurn() ? continuePlayerTurn() : setUpSimonTurn();
-		}
-
-		function isStillPlayersTurn () {
-			return player.clickLog.length < simon.clickLog.length;
-		}
-
-		function continuePlayerTurn () {
-			player.clickNumber++;
-			playerClick();
-		}
-
-		function increaseGameRound () {
-			game.round++;
-		}
-
-		function resetPlayerTurnVariables () {
-			player.clickLog.length = 0;
-			player.clickNumber = 0;
-		}
-
-		function setUpSimonTurn () {
-			resetPlayerTurnVariables();
-			board.increasePlayerScoreDisplay(player.name, game.round);		
-			simonTurn();
-			increaseGameRound();
-		}
-
-		function simonTurn(){
-			setTimeout(function(){
-				simon.button = board.generateSimonButton();
-				simon.logColour();
-				board.simonFlashSound(simon.clickLog);					//registers computer choice with a flash and a sound
-				playerClick();																	//calls player's turn 
-			}, 2000);																		
-		};
-
-		function handleIncorrectChoice(){
-			board.alertError();
-			playAgainQuestion();
-		}
-
-		function playAgainQuestion(){
-			$(document).keypress(function(event){
-				if (playerAnswersYes(event)) {
-					handlePlayerSelectingYes();
-				}
-				if (playerAnswersNo(event)) {
-					handlePlayerSelectingNo();
-				}
-			})
-		}
-
-		function playerAnswersYes (event) {
-			return event.which == 89 || event.which == 121;
-		}
-
-		function playerAnswersNo (event) {
-			return event.which == 78 || event.which == 110;
-		}
-
-		function handlePlayerSelectingYes () {
-			hideQuestion();
-			setUpRepeatGame();					
+	function setUpPlayerNameHandler (event) {
+		player.name = $('#nameInput').val().toLowerCase();
+		if (event.which == 13) {
 			startGame();
 		}
-
-		function handlePlayerSelectingNo () {
-			hideQuestion();
-			restart();
-		}
-
-		function hideQuestion() {
-			$('#question').text('');
-			$('#question').slideUp();
-		}
-
-		function restart(){
-			setTimeout(function(){
-				board.showInfoWindow();
-				resetGameVariables();
-				startIntro();
-			}, 2000);
-		}
-
-		function resetGameVariables(){
-			createNewGameAndNewSimon();
-			createNewPlayer();
-		}
-
-		function setUpRepeatGame(){
-			createNewPlayerAndNewSimon();
-			resetPlayer();
-		}
-
-		function createNewGameAndNewSimon() {
-			game = new Game();
-			simon = new Simon();
-		}
-
-		function createNewPlayer () {
-			player = new Player();
-		}
-
-		function resetPlayer() {
-			player.click;				//player choice
-			player.clickLog = [];		//log of player choices
-			player.clickNumber = 0;		//counter for assessEachClick()
-		}
 	}
+
+	function startGame(){
+		board.clearNameInputWindow();
+		board.setUpGame(player.name);
+		simonTurn();	  	
+	}
+
+	function playerClick(){
+		$('input').each(function(){
+			$(this).on('click', function(){
+				preventMultipleClick();
+				handlePlayerClick($(this));
+			})
+		})
+	};
+
+	function preventMultipleClick () {
+		$('input').off('click');
+	}
+
+	function handlePlayerClick (selectedElement) {
+		board.registerPlayerClick(selectedElement.attr('class'));
+		player.clickLog.push(selectedElement.attr('class'));
+		assessClick();
+	}
+
+	function assessClick(){
+		isPlayerClickCorrect() ? handleCorrectChoice() : handleIncorrectChoice();	
+	}
+
+	function isPlayerClickCorrect () {
+		return (player.clickLog[player.clickNumber] == simon.clickLog[player.clickNumber]);
+	}
+
+	function handleCorrectChoice () {
+		isStillPlayersTurn() ? continuePlayerTurn() : setUpSimonTurn();
+	}
+
+	function isStillPlayersTurn () {
+		return player.clickLog.length < simon.clickLog.length;
+	}
+
+	function continuePlayerTurn () {
+		player.clickNumber++;
+		playerClick();
+	}
+
+	function increaseGameRound () {
+		game.round++;
+	}
+
+	function resetPlayerTurnVariables () {
+		player.clickLog.length = 0;
+		player.clickNumber = 0;
+	}
+
+	function setUpSimonTurn () {
+		resetPlayerTurnVariables();
+		board.increasePlayerScoreDisplay(game.round);
+		simonTurn();
+		increaseGameRound();
+	}
+
+	function simonTurn(){
+		setTimeout(function(){
+			simon.button = board.generateSimonButton();
+			simon.logColour();
+			board.executeSimonTurn(simon.clickLog);					//registers computer choice with a flash and a sound
+			playerClick();																	//calls player's turn 
+		}, 2000);																		
+	};
+
+	function handleIncorrectChoice(){
+		board.alertError();
+		playAgainQuestion();
+	}
+
+	function playAgainQuestion(){
+		$(document).keypress(function(event){
+			if (playerAnswersYes(event)) {
+				handlePlayerSelectingYes();
+			}
+			if (playerAnswersNo(event)) {
+				handlePlayerSelectingNo();
+			}
+		})
+	}
+
+	function playerAnswersYes (event) {
+		return event.which == 89 || event.which == 121;
+	}
+
+	function playerAnswersNo (event) {
+		return event.which == 78 || event.which == 110;
+	}
+
+	function handlePlayerSelectingYes () {
+		hideQuestion();
+		setUpRepeatGame();					
+		startGame();
+	}
+
+	function handlePlayerSelectingNo () {
+		hideQuestion();
+		restart();
+	}
+
+	function hideQuestion() {
+		$('#question').text('');
+		$('#question').slideUp();
+	}
+
+	function restart(){
+		setTimeout(function(){
+			board.showInfoWindow();
+			resetGameVariables();
+			startIntro();
+		}, 2000);
+	}
+
+	function resetGameVariables(){
+		createNewGameAndNewSimon();
+		createNewPlayer();
+	}
+
+	function setUpRepeatGame(){
+		createNewPlayerAndNewSimon();
+		resetPlayer();
+	}
+
+	function createNewGameAndNewSimon() {
+		game = new Game();
+		simon = new Simon();
+	}
+
+	function createNewPlayer () {
+		player = new Player();
+	}
+
+	function resetPlayer() {
+		// player.click;				//player choice
+		player.clickLog = [];		//log of player choices
+		player.clickNumber = 0;		//counter for assessEachClick()
+	}
+
+	init();
+
 });
